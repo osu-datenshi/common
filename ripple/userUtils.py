905b239afd651fff0a5c913eeba11a351949215b
Rei Hakurei <reimu_after_marisa@yahoo.com>
2021-01-03 21:53:23 +0700

    reiturn None

--- ripple/userUtils.py
+++ ripple/userUtils.py
@@ -1494,6 +1494,7 @@ def verifyUser(userID, hashes):
 		# No matches found, set USER_PUBLIC and USER_NORMAL flags and reset USER_PENDING_VERIFICATION flag
 		resetPendingFlag(userID)
 		#log.info("User **{}** ({}) has verified his account with hash set _{}_".format(username, userID, hashes[2:5]), "cm")
+        return True
 
 
 def hasVerifiedHardware(userID):
