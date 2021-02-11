842e01d0f035fb8780e7891b4774bdc0f3c28833
Rei Hakurei <reimu_after_marisa@yahoo.com>
2021-02-11 16:54:43 +0700

    move back recalc ranked score

--- constants/features.py
+++ constants/features.py
@@ -5,6 +5,6 @@ GLOBAL_COUNTRY_RANKS  = 'country'
 # allow V2 leaderboard
 RANKING_SCOREV2 = False
 # enforce recalc ranked score
-RANKED_SCORE_RECALC = 1613062800
+RANKED_SCORE_RECALC = 1613235600
 # once ready to full deprecate it, put it in here.
 DEPRECATE_SHITTY_ABBREVIATION = []
