42728fe598b8b0a728b38bcba41aa855c6c87163
Rei Hakurei <reimu_after_marisa@yahoo.com>
2021-05-11 23:59:39 +0700

    add master user table on switches

--- datenshi/modeSwitches.py
+++ datenshi/modeSwitches.py
@@ -9,17 +9,18 @@ from common.ripple import passwordUtils, scoreUtils
 from objects import glob
 
 def __wrapper__():
-    h = {}
-    tbl_mode   = namedtuple('tbl_mode', 'std relax alt')
-    h['stats'] = tbl_mode('users_stats', 'rx_stats', 'alternative_stats')
-    h['score'] = tbl_mode('scores', 'scores_relax', 'scores_alternative')
-    h['beatmap_pc'] = tbl_mode('users_beatmap_playcount', 'rx_beatmap_playcount', 'users_beatmap_playcount')
-    h['leaderboard'] = tbl_mode('leaderboard', 'leaderboard_relax', 'leaderboard_alt')
-    h['rp_folder'] = tbl_mode('', '_relax', '_alt')
-    
-    for k,v in h.items():
-        globals()[k] = v
-    del globals()['__wrapper__']
+	h = {}
+	tbl_mode   = namedtuple('tbl_mode', 'std relax alt')
+	if not features.MASTER_USER_TABLE:
+		h['stats'] = tbl_mode('users_stats', 'rx_stats', 'alternative_stats')
+	h['score'] = tbl_mode('scores', 'scores_relax', 'scores_alternative')
+	h['beatmap_pc'] = tbl_mode('users_beatmap_playcount', 'rx_beatmap_playcount', 'users_beatmap_playcount')
+	h['leaderboard'] = tbl_mode('leaderboard', 'leaderboard_relax', 'leaderboard_alt')
+	h['rp_folder'] = tbl_mode('', '_relax', '_alt')
+	
+	for k,v in h.items():
+		globals()[k] = v
+	del globals()['__wrapper__']
 __wrapper__()
 
 del namedtuple
