bba96b580da0cc756928cdfce2e7adc66aea1fc4
Rei Hakurei <reimu_after_marisa@yahoo.com>
2021-06-04 16:44:50 +0700

    add replacement

--- datenshi/reigexp.py
+++ datenshi/reigexp.py
@@ -1,11 +1,26 @@
 import re
 
-def match(type, search, string):
+matchFlags = re.M
+
+def match(type, search, string, flags=matchFlags):
 	if type == 'split':
-		return bool(re.search('{1}{2}{1}'.format(r'\b', search), string))
+		return bool(re.search('{1}{2}{1}'.format(r'\b', search), string, flags=flags))
 	elif type in ('advanced', 'regexp'):
-		return bool(re.search(search, string))
+		return bool(re.search(search, string, flags=flags))
 	elif type == 'partial':
 		return search in string
 	else:
 		return search == string
+
+def replace(type, search, string, replacement, flags=matchFlags):
+	if not match(type, search, string):
+		return string
+	if type == 'split':
+		split_search = '{1}{2}{1}'.format(r'\b', search)
+		return re.sub(split_search, replacement, string, flags=flags)
+	elif type in ('advanced', 'regexp'):
+		return re.sub(search, replacement, string, flags=flags)
+	elif type == 'partial':
+		return string.replace(search, replacement)
+	else:
+		return replacement
